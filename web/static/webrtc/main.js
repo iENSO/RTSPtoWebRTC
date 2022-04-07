58c4384eaab4f34f3bf3677e7cc487e8e06f9c9d
DEVALORE\vvolchko <vladi.vol@devalore.com>
2022-04-07 11:09:46 +0300

    removed some logs + offer to receive video

--- web/static/webrtc/main.js
+++ web/static/webrtc/main.js
@@ -29,53 +29,37 @@ const startListening = (iensoWssToken) => {
             "iceCandidatePoolSize": "0"
         })
         const pc = connections["sessionId"];
-        pc.addTransceiver("video", {
-            'direction': 'sendrecv'
-        })
         pc.ontrack = function (event) {
-            console.log("AAAAAAAAAAAA(ontrack)")
             stream.addTrack(event.track);
             document.getElementById("videoElem").srcObject = stream;
         }
-        pc.onnegotiationneeded = async () => {
-            console.log("onnegotiationneeded")
-            const offer = await pc.createOffer()
-            await pc.setLocalDescription(offer)
-            const encodedOffer = {
-                type: offer.type,
-                sdp: window.btoa(offer.sdp)
-            }
-            socket.send(JSON.stringify(encodedOffer));
-        }
+        // pc.onnegotiationneeded = async () => {
+        //     console.log("onnegotiationneeded")
+        //     const offer = await pc.createOffer()
+        //     await pc.setLocalDescription(offer)
+        //     const encodedOffer = {
+        //         type: offer.type,
+        //         sdp: window.btoa(offer.sdp)
+        //     }
+        //     socket.send(JSON.stringify(encodedOffer));
+        // }
         pc.onicecandidate = ({candidate}) => {
             if (candidate) {
                 console.log("Generated ICE Candidate", candidate);
                 socket.send(JSON.stringify(candidate));
             }
         }
+        const offer = await pc.createOffer({
+            offerToReceiveVideo: true,
+        })
+        await pc.setLocalDescription(offer)
+        const encodedOffer = {
+            type: offer.type,
+            sdp: window.btoa(offer.sdp)
+        }
+        socket.send(JSON.stringify(encodedOffer));
     }
 
-}
-
-
-const handleSignalingOffer = async (sessionId, offer) => {
-    const pc = connections[sessionId]
-    console.log("handleSignalingAnswer", offer);
-    const decodedOffer = {
-        type: offer.type,
-        sdp: window.atob(offer.sdp)
-    }
-    await pc.setRemoteDescription(decodedOffer);
-
-    return;
-    const answer = await pc.createAnswer();
-    await pc.setLocalDescription(answer);
-    const encodedAnswer = {
-        type: answer.type,
-        sdp: window.btoa(answer.sdp)
-    }
-    console.log("Sending Answer", answer)
-    socket.send(JSON.stringify(encodedAnswer));
 }
 
 let candidateCache = [];
@@ -106,7 +90,6 @@ const handleSignalingCandidate = async (sessionId, candidate) => {
 const handleSignalingMessage = async (message, socket) => {
     const data = JSON.parse(message.data);
     const {type, payload} = data;
-
     const sessionId = "sessionId";
     if (type === "answer") {
         handleSignalingAnswer(sessionId, {
@@ -115,20 +98,12 @@ const handleSignalingMessage = async (message, socket) => {
         })
         return;
     }
-
-
     if (data.candidate) {
         handleSignalingCandidate(sessionId, data)
         return;
     }
 
     console.log("Unknown Message", data);
-    // if (payload.type === "offer" ) {
-    //     handleSignalingOffer(sessionId, payload)
-    //     return;
-    // }
-
-
 }
 
 generateSessionToken()
